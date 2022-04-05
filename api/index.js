fe0442cbdbbc95d26f2cd8259e75dd716e73f6ea
Jeremy Menard <menard.jeremy@gmail.com>
2022-04-05 18:06:25 -0400

    update 15

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 app.use(cors({
   // origin: 'https://polarityworks.github.io'
   //origin: 'http://localhost:8080'
-  origin: 'https://menjeremy.github.io/keymapapp'//'http://localhost'
+  origin: 'https://menjeremy.github.io'//'http://localhost'
 }))
 
 if (process.env.ENABLE_DEV_SERVER) {
